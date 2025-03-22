5660943fad2d9bc38d61a1f705460b38982699e8
Ben Rosen <benjamin.j.rosen@gmail.com>
2025-03-21 20:29:15 -0400

    feat: init highlighting

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,2 +1,11 @@
+(class_identifier) @constructor
 (comment) @comment
+(duration_identifier) @variable.builtin
+(float) @number
+(global_unit_generator) @keyword
+(identifier) @variable
+(int) @number
+(member_identifier member_identifier: (identifier) @property)
+(now_keyword) @keyword
 (string) @string
+(type) @type
