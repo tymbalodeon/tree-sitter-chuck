22e1754150c5220a73146dfec31332db3bf4cdf6
Ben Rosen <benjamin.j.rosen@gmail.com>
2025-03-27 21:38:48 -0400

    feat: update highlight queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,10 +1,12 @@
-(class_identifier) @unit-generator
+(chuck_operator) @operator
+(class_identifier) @label
 (comment) @comment
 (duration_identifier) @type
-(float) @number
-(global_unit_generator) @keyword
-(hexidecimal) @number
-(int) @number
+(float) @constant.numeric.float
+(global_unit_generator) @variable.builtin
+(hexidecimal) @constant.numeric
+(int) @constant.numeric.integer
 (keyword) @keyword
+(operator) @operator
 (primitive_type) @type
 (string) @string
